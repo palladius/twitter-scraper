761e7900a346fc2e3bec55746109da68998b9db8
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-02-06 07:59:51 +0100

    and first graphs are out!

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def stats
+  end
+
+  def about
+  end
+
+  def wordle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   resources :twitter_users
   resources :comments
   resources :posts
+
+  get "/articles", to: "articles#index"
+  get "/articles/stats", to: "articles#stats"
+  get "/stats", to: "articles#stats"
+  get "/wordle", to: "articles#wordle"
+  #bin/rails generate controller Articles index stats about wordle --skip-routes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
