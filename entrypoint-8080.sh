c92ae551710c503f37f51f50b3120d8eb6c09c18
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-02-09 23:18:40 +0100

    minor

--- entrypoint-8080.sh
+++ entrypoint-8080.sh
@@ -2,10 +2,10 @@
 
 export RAILS_ENV=production
 
-echo "[ENTRYPOINT8080] Im not sure if I should have .env baked INSIDE the docker image or not. I presume I should NOT right? Well then let me add it to .dockerignore then"
-ls -al 
-echo "[ENTRYPOINT8080] MESSAGGIO_OCCASIONALE: $MESSAGGIO_OCCASIONALE"
-echo "[ENTRYPOINT8080] DATABASE_URL REMOVEME: $DATABASE_URL"
+echo "[ENTRYPOINT8080][RAILS_ENV=$RAILS_ENV] Im not sure if I should have .env baked INSIDE the docker image or not. I presume I should NOT right? Well then let me add it to .dockerignore then"
+#ls -al 
+echo "[ENTRYPOINT8080][RAILS_ENV=$RAILS_ENV] MESSAGGIO_OCCASIONALE: $MESSAGGIO_OCCASIONALE"
+#echo "[ENTRYPOINT8080] DATABASE_URL REMOVEME: $DATABASE_URL"
 
 rake db:migrate
 rake assets:precompile
