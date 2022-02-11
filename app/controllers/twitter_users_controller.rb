5a855229ccd8868de1bba91dbb81a369a7b58165
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-02-11 22:13:38 +0100

    works in docker prod local. lets push!

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -5,8 +5,7 @@ class TwitterUsersController < ApplicationController
   def index
     #@twitter_users = #TwitterUser.all.limit(150).sort()
     # getting top users
-    @twitter_users = TwitterUser.left_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC').limit(150)
-
+    @twitter_users = TwitterUser.left_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC').limit(50)
   end
 
   # GET /twitter_users/1 or /twitter_users/1.json
