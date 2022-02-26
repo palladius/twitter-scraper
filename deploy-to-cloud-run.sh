dfc1edddf0532af744f35ff22a188636e69866cf
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-02-26 09:19:17 +0100

    bello

--- deploy-to-cloud-run.sh
+++ deploy-to-cloud-run.sh
@@ -3,9 +3,9 @@
 source .env
 
 # 1.3 Had to move DATABASE_URL to PROD_DATABASE_URL or DEV would pick it up and commit to PROD DB by mistake !!! (which is WAI)
+# 1.5 Added TWITTER vars so now i can call updates from Cloud Fujnction and not have a client running forever :)
 
-
-DEPLOY_VERSION="1.4_20220213"
+DEPLOY_VERSION="1.5_20220226"
 VERSION=`cat VERSION`
 PROJECT_ID=$(gcloud config get-value project)
 
@@ -17,6 +17,8 @@ echodo gcloud run deploy twitter-scraper \
      --set-env-vars="DATABASE_URL=$PROD_DATABASE_URL" \
      --set-env-vars="PROD_DATABASE_URL=$PROD_DATABASE_URL" \
      --set-env-vars="APP_VERSION=$VERSION" \
+     --set-env-vars="TWITTER_CONSUMER_KEY=$TWITTER_CONSUMER_KEY" \
+     --set-env-vars="TWITTER_CONSUMER_SECRET=$TWITTER_CONSUMER_SECRET" \
      --set-env-vars="APP_LOCATION=GCP CloudRun on $PROJECT_ID (prod)" \
      --platform managed \
      --region europe-west1 \
