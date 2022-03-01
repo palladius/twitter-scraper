56fecfe1a0563ddec880512be08e40497fe31597
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-03-01 19:39:00 +0100

    refactored db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,91 +17,88 @@ require 'twitter'
 # v4 2022-02-18: Added POLYMOPRH_BEGIN to Tweet - adding from object i seralized in tmp/marshal/ WOOT!
 # v5 2022-02-19: moved code execution in model/concerns so it can be run also by a Runner. changed just description and debugging output.
 
-$n_tweets = (ENV["TWITTER_INGEST_SIZE"] || '42' ).to_i
-$rake_seed_import_version = "5"
-$check_already_exists = true
-$lets_try_async_runners = Rails.env == 'development' # only in dev
-$hostname = Socket.gethostname.split('.')[0] rescue "hostname_error" #shotname
-$search_terms = [
-  'joguei https://t.co',
-  'k4rlheinz', # Julio
-  '#TwitterParser',
-  '#Wordle',
-   '@dewordle', # 
-  '#Parole',
-  'mathler.com',
-  'mathler',
-#  'https://term.ooo/',
-  'wordlefr',
-  'WordleIT',
-  'wordlept',
-  'wordle.uber.space',
-  'Par🇮🇹le',
-  'I guessed this 5-letter word in', #  'wordlegame.org', # since 0.11 I support it!
-  '#taylordle',
-  'katapat',
-  'worldle',
-  'wekele',
-  'quordle',
-  # this works but produces TOO much and i dont know what it i
-  #'🟩🟩🟩🟩🟩', # success
-  # We keep this last
- 'Wordle',
-]
-$async_wordle_search_terms = %w{ wordle wordlees wordleo #wordleparser }
-$marshal_on_file = (ENV["MARSHAL_TO_FILE"] =='true' || false ) rescue false
-# main
-
 def db_seed_puts(str)
   common_header = "[#{white 'DB:SEED'}][#{yellow Rails.env.first(4)}] "
   puts "#{common_header}#{str}"
 end
 
 def main
+
+  intermediate_search_term = ENV.fetch 'SEARCH_ONLY_FOR', nil
+  n_tweets = (ENV["TWITTER_INGEST_SIZE"] || '42' ).to_i
+  lets_try_async_runners = Rails.env == 'development' # only in dev
+  async_wordle_search_terms = %w{ wordle wordlees wordleo #wordleparser }
+  marshal_on_file = (ENV["MARSHAL_TO_FILE"] =='true' || false ) rescue false
+  search_terms = [
+    'joguei https://t.co',
+    'k4rlheinz', # Julio
+    '#TwitterParser',
+    '#Wordle',
+    '@dewordle', # 
+    '#Parole',
+    'mathler.com',
+    'mathler',
+    '#mathler',
+  #  'https://term.ooo/',
+    'wordlefr',
+    'WordleIT',
+    'wordlept',
+    'wordle.uber.space',
+    'Par🇮🇹le',
+    'I guessed this 5-letter word in', #  'wordlegame.org', # since 0.11 I support it!
+    '#taylordle',
+    'katapat',
+    'worldle',
+    'wekele',
+    'quordle',
+    # this works but produces TOO much and i dont know what it i
+    #'🟩🟩🟩🟩🟩', # success
+    # We keep this last
+    (Rails.env == 'development' ? nil : 'Wordle'),
+  ].select{|x| not x.nil?}
+
+
+
   db_seed_puts "TWITTER_CONSUMER_KEY: #{white ENV['TWITTER_CONSUMER_KEY']}. If this was empty, good luck using APIs!"
   raise "Missing Twitter key (or exists but is smaller than XX chars: #{ENV['TWITTER_CONSUMER_KEY'].to_s.length}" if ENV['TWITTER_CONSUMER_KEY'].to_s.length < 5
   #db_seed_puts "TEST REMOVEME quando va: ", WordleTweet.extended_wordle_match_type("ciao da Riccardo")
-  db_seed_puts "N_ITERATIONS from ENV: #{yellow $n_tweets}"
-  db_seed_puts "#{white $search_terms.count.to_s } search terms: #{yellow $search_terms}"
+  db_seed_puts "N_ITERATIONS from ENV: #{yellow n_tweets}"
+  db_seed_puts "#{white search_terms.count.to_s } search terms: #{yellow search_terms}"
   db_seed_puts "Ingesting into this DB: #{yellow Rails.configuration.database_configuration[Rails.env]["adapter"]}"
   db_seed_puts "Stats: #{yellow Tweet.count.to_s} tweets, #{yellow WordleTweet.count.to_s} WTs, #{yellow TwitterUser.count.to_s}  Users"
-  db_seed_puts "Stats: $marshal_on_file activated! Look into logs/ " if $marshal_on_file
+  db_seed_puts "Stats: marshal_on_file activated! Look into logs/ " if marshal_on_file
   #exit 42
 
-  raise "Too few tweets required. Set TWITTER_INGEST_SIZE env var!" if $n_tweets < 1
-  if $lets_try_async_runners
-    $async_wordle_search_terms.each do |delayed_word|
+  raise "Too few tweets required. Set TWITTER_INGEST_SIZE env var!" if n_tweets < 1
+  if lets_try_async_runners
+    async_wordle_search_terms.each do |delayed_word|
       Tweet.delay.seed_by_calling_twitter_apis(
         delayed_word,
-        $n_tweets, {
+        n_tweets, {
           :description => 'DELAYED call from db:seed'
       })
       end
   else
       puts "We'll skip async runners since they seem to clog in dev and return TOO MANY APU CALLS :) and kill everyone else :)"
     end
+  #puts(green("This is the new code! It uses the Tweet.first.load_from_twitter concern - wOOt!"))
+  #puts "Looking for #{n_tweets} tweets matching #Wordle hashtag:"
 
-  rake_seed_parse_keys_simplified
-end
-
-def rake_seed_parse_keys_simplified()
-  puts(green("This is the new code! It uses the Tweet.first.load_from_twitter concern - wOOt!"))
-
-  puts "Looking for #{$n_tweets} tweets matching #Wordle hashtag:"
-
-  intermediate_search_term = ENV.fetch 'SEARCH_ONLY_FOR', nil
+  
   search_terms = intermediate_search_term.nil? ?
-    $search_terms :               # Global thingy
+    search_terms :               # Global thingy
     [ intermediate_search_term]   # local if you provide $SEARCH_ONLY_FOR
   puts "Searching for these keywords: #{azure search_terms.join(', ')}"
   debug = ENV.fetch 'DEBUG', nil
+  opts = {
+    :description => 'DIRECT (non-delayed) call from db:seed',
+    :debug => (not debug.nil?) ,
+  }
   search_terms.each do |search_term|
     ret = Tweet.seed_by_calling_twitter_apis(
       search_term,
-      $n_tweets, {
-        :description => 'DIRECT (non-delayed) call from db:seed',
-        :debug => (not debug.nil?) ,
-    })
+      n_tweets, 
+      opts)
     puts "rake_seed_parse_keys_simplified: ret=#{ret}"
   end
 
