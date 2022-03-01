4b4b98e7e4773bef92daf355fb32754bf2a1d339
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2022-03-01 07:12:47 +0100

    added also redis

--- config/initializers/riccardo.rb
+++ config/initializers/riccardo.rb
@@ -19,6 +19,12 @@ APP_VERSION = ENV["APP_VERSION"] ?
 addon =  Rails.application.config_for(:wordle_regexes, env: "addon_in_the_end")
 WORDLE_REGEXES = Rails.application.config_for(:wordle_regexes, env: "default").concat(addon).flatten.select{|el| el.key?(:return) rescue false}
 
-
+# #if [ -f /.dockerenv ]; then
+# echo "I'm inside matrix ;(";
+# else
+#     echo "I'm living in real world!";
+# fi
+# https://stackoverflow.com/questions/23513045/how-to-check-if-a-process-is-running-inside-docker-container
+AM_I_ON_DOCKER = File.exists?('/.dockerenv')
 # yellow() defined in config/boot
 
